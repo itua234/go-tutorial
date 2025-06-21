974a73de31a88a8183839e78e1976c33b290a7eb
osemeilu itua <ituaosemeilu234@gmail.com>
2025-06-21 23:54:43 +0100

    modified the authappbysecretkey middleware

--- middlewares/auth_middleware.go
+++ middlewares/auth_middleware.go
@@ -16,30 +16,28 @@ import (
 func AuthenticateAppBySecretKey(db *gorm.DB) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		startTime := time.Now()
-		rawToken := c.GetHeader("x-allow-key")
-		token := ""
-		if rawToken != "" {
-			// If multiple headers, use the first
-			if strings.Contains(rawToken, ",") {
-				tokens := strings.Split(rawToken, ",")
-				token = strings.TrimSpace(tokens[0])
-				log.Printf("Multiple API keys found in 'x-allow-key' header, using the first one.")
-			} else {
-				token = rawToken
-			}
-		}
 		requestMethod := c.Request.Method
 		requestUrl := c.Request.RequestURI
 		clientIp := c.ClientIP()
 		log.Printf("Request received: %s %s from %s", requestMethod, requestUrl, clientIp)
 
+		rawToken := c.GetHeader("x-api-key")
+		token := ""
+		if rawToken != "" {
+			if strings.Contains(rawToken, ",") {
+				tokens := strings.Split(rawToken, ",")
+				token = strings.TrimSpace(tokens[0])
+				log.Printf("Multiple API keys found in 'x-api-key' header, using the first one.")
+			} else {
+				token = rawToken
+			}
+		}
 		if token == "" {
 			log.Printf("Authentication failed: Missing API key.")
-			c.JSON(http.StatusUnauthorized, gin.H{"error": true, "message": "unauthenticated"})
+			c.JSON(http.StatusUnauthorized, gin.H{"error": true, "message": "Authentication failed: Missing API key."})
 			c.Abort()
 			return
 		}
-
 		log.Printf("Attempting authentication with key: %s... (masked)", maskToken(token))
 
 		// Lookup appId in Redis
